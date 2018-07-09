79a8783952f12bb455c4af2c24051b1f08842910
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-09 03:41:52 -0400

    illuminate/database

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,6 +8,7 @@ class ComposerStaticInit59feb54a1fd2036e9e66eaa8912e6bbb
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        '72579e7bd17821bb1321b87411366eae' => __DIR__ . '/..' . '/illuminate/support/helpers.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -22,10 +23,24 @@ class ComposerStaticInit59feb54a1fd2036e9e66eaa8912e6bbb
         'S' => 
         array (
             'Symfony\\Polyfill\\Mbstring\\' => 26,
+            'Symfony\\Component\\Translation\\' => 30,
         ),
         'P' => 
         array (
+            'Psr\\SimpleCache\\' => 16,
+            'Psr\\Container\\' => 14,
             'Phroute\\Phroute\\' => 16,
+        ),
+        'I' => 
+        array (
+            'Illuminate\\Support\\' => 19,
+            'Illuminate\\Database\\' => 20,
+            'Illuminate\\Contracts\\' => 21,
+            'Illuminate\\Container\\' => 21,
+        ),
+        'D' => 
+        array (
+            'Doctrine\\Common\\Inflector\\' => 26,
         ),
     );
 
@@ -42,10 +57,46 @@ class ComposerStaticInit59feb54a1fd2036e9e66eaa8912e6bbb
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
         ),
+        'Symfony\\Component\\Translation\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/translation',
+        ),
+        'Psr\\SimpleCache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/simple-cache/src',
+        ),
+        'Psr\\Container\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/container/src',
+        ),
         'Phroute\\Phroute\\' => 
         array (
             0 => __DIR__ . '/..' . '/phroute/phroute/src/Phroute',
         ),
+        'Illuminate\\Support\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/illuminate/support',
+        ),
+        'Illuminate\\Database\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/illuminate/database',
+        ),
+        'Illuminate\\Contracts\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/illuminate/contracts',
+        ),
+        'Illuminate\\Container\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/illuminate/container',
+        ),
+        'Doctrine\\Common\\Inflector\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/inflector/lib/Doctrine/Common/Inflector',
+        ),
+    );
+
+    public static $fallbackDirsPsr4 = array (
+        0 => __DIR__ . '/..' . '/nesbot/carbon/src',
     );
 
     public static $prefixesPsr0 = array (
@@ -63,6 +114,7 @@ class ComposerStaticInit59feb54a1fd2036e9e66eaa8912e6bbb
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit59feb54a1fd2036e9e66eaa8912e6bbb::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit59feb54a1fd2036e9e66eaa8912e6bbb::$prefixDirsPsr4;
+            $loader->fallbackDirsPsr4 = ComposerStaticInit59feb54a1fd2036e9e66eaa8912e6bbb::$fallbackDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit59feb54a1fd2036e9e66eaa8912e6bbb::$prefixesPsr0;
 
         }, null, ClassLoader::class);
