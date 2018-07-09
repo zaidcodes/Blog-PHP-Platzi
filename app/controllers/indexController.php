4e05aa5ac2524336e55bc3f553938a22fbea6589
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-09 18:44:19 -0400

    BlogPost Pagination

--- app/controllers/indexController.php
+++ app/controllers/indexController.php
@@ -5,9 +5,20 @@ use app\models\BlogPost;
 
 class IndexController extends BaseController{
 
-    public function getIndex(){
-        $blogPosts = BlogPost::query()->orderBy('created_at','DESC')->get();
-        //'SELECT post_title, Date_format(post_created_at,"%d-%M-%Y") as post_created_at, post_created_by, post_image, post_content  FROM blog_post ORDER BY post_created_at DESC'
-        return $this->render('index', ['blogPosts'=>$blogPosts]);
+    public function getIndex($page = 1){
+
+        $limit = 5;
+        
+        $totalReg = BlogPost::all()->count();
+        $totalPages = ceil($totalReg / $limit);
+        $skip = ($limit * $page ) - $limit;
+
+        $blogPosts = BlogPost::query()->orderBy('created_at','DESC')->skip($skip)->take($limit)->get();
+
+        return $this->render('index', [
+        	'blogPosts' => $blogPosts,
+        	'totalPages' => $totalPages,
+        	'page' => $page
+        ]);
     }
 }
