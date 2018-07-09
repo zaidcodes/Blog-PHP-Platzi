be4775e7d808d78341ef761e5f1da3063a6b3ed9
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-09 04:39:23 -0400

    Database sintax Eloquent

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
 require_once '../vendor/autoload.php';
-include_once('../config.php');
 
 $baseUrl = '';
 $baseDir = str_replace(basename($_SERVER['SCRIPT_NAME']),'',$_SERVER['SCRIPT_NAME']); 
@@ -13,12 +12,23 @@ define('BASE_URL',$baseUrl);
 
 $route = $_GET['route'] ?? '/';
 
-function render($fileName, $params = []){
-    ob_start();
-    extract($params);
-    include $fileName;
-    return ob_get_clean();
-}
+//Starting Database
+use Illuminate\Database\Capsule\Manager as Capsule;
+$capsule = new Capsule;
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'blog-php-platzi',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_bin',
+    'prefix'    => '',
+]);
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
 
 use Phroute\Phroute\RouteCollector;
 $router = new RouteCollector();
