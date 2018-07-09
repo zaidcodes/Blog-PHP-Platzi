c6fbce5dde37344143279c6e841f82e93ae4f42c
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-09 04:40:02 -0400

    Changing names

---
+++ app/models/BlogPost.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\models;
+use Illuminate\Database\Eloquent\Model;
+
+class BlogPost extends Model{
+    protected $table = 'blog_post';
+    protected $fillable = ['title','content','created_by'];
+}
