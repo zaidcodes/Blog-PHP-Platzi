140685fccf6dab71987dc712a45a632c655538f1
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-09 04:38:49 -0400

    Query sintax eloquent

--- app/controllers/admin/PostController.php
+++ app/controllers/admin/PostController.php
@@ -3,15 +3,13 @@
 namespace app\controllers\admin;
 
 use app\controllers\BaseController;
+use app\models\BlogPost;
 
 class PostController extends BaseController{
 
     public function getIndex(){
-        global $pdo;
-
-        $query = $pdo->prepare('SELECT Date_format(post_created_at,"%d-%M-%Y") as post_created_at, post_created_by, post_title FROM blog_post ORDER BY post_created_at,post_created_by ASC');
-        $query->execute();
-        $blogPosts = $query->fetchAll(\PDO::FETCH_ASSOC);
+        $blogPosts = BlogPost::all();
+        //'SELECT Date_format(post_created_at,"%d-%M-%Y") as post_created_at, post_created_by, post_title FROM blog_post ORDER BY post_created_at,post_created_by ASC'
         return $this->render('admin/posts', ['blogPosts'=>$blogPosts]);
     }
 
@@ -20,16 +18,13 @@ class PostController extends BaseController{
     }
     
     public function postCreate(){
-        global $pdo;
-
-        $result = false;
-        $sql = "INSERT INTO blog_post (post_title, post_content, post_created_by) VALUES (:title, :content, :author);";
-        $query = $pdo->prepare($sql);
-        $result = $query->execute([
+        $blogPost = new BlogPost([
             'title' => $_POST['inputTitle'],
             'content' => $_POST['inputContent'],
-            'author' => $_POST['inputAuthor']
+            'created_by' => $_POST['inputAuthor']
         ]);
+        $blogPost->save();
+        $result = true;
         return $this->render('admin/insert-post', ['result'=>$result]);
     }
 
--- app/controllers/indexController.php
+++ app/controllers/indexController.php
@@ -1,16 +1,13 @@
 <?php
 
 namespace app\controllers;
+use app\models\BlogPost;
 
 class IndexController extends BaseController{
 
     public function getIndex(){
-        global $pdo;
-
-        $query = $pdo->prepare('SELECT post_title, Date_format(post_created_at,"%d-%M-%Y") as post_created_at, post_created_by, post_image, post_content  FROM blog_post ORDER BY post_created_at DESC');
-        $query->execute();
-        $blogPosts = $query->fetchAll(\PDO::FETCH_ASSOC);
-
+        $blogPosts = BlogPost::query()->orderBy('created_at','DESC')->get();
+        //'SELECT post_title, Date_format(post_created_at,"%d-%M-%Y") as post_created_at, post_created_by, post_image, post_content  FROM blog_post ORDER BY post_created_at DESC'
         return $this->render('index', ['blogPosts'=>$blogPosts]);
     }
 }
