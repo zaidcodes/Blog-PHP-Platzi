dad1c718fc6209b7eae19b2d1714ee0a02c77937
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-08 23:13:16 -0400

    Deleting php script

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<?php
-$query = $pdo->prepare('SELECT post_title, Date_format(post_created_at,"%d-%M-%Y") as post_created_at, post_created_by, post_image, post_content  FROM blog_post ORDER BY post_created_at DESC');
-$query->execute();
-
-$blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
-
-
-?>
-
-
 <html lang="en">
 <head>
     <meta charset="UTF-8">
