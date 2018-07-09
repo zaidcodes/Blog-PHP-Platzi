463d54c8480638e0accdb3002ad579e68669b626
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-08 23:00:17 -0400

    adding "composer autoload" and deleting "route example"

--- public/index.php
+++ public/index.php
@@ -3,23 +3,9 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
+require_once '../vendor/autoload.php';
+
 include_once('../config.php');
 
 $route = $_GET['route'] ?? '/';
 
-switch ($route) {
-    case '/':
-        require '../index.php';
-        break;
-    case '/admin':
-        require '../admin/index.php';
-        break;
-    case '/admin/posts':
-        require '../admin/posts/index.php';
-        break;
-    
-    
-    default:
-        # code...
-        break;
-}
