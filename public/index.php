4d34f4f55b0b80a02525579a3253f40b01dd19f5
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-09 00:18:17 -0400

    adding route /admin/posts/create

--- public/index.php
+++ public/index.php
@@ -22,15 +22,18 @@ function render($fileName, $params = []){
 
 use Phroute\Phroute\RouteCollector;
 $router = new RouteCollector();
+
 $router->get('/',function() use($pdo){
     $query = $pdo->prepare('SELECT post_title, Date_format(post_created_at,"%d-%M-%Y") as post_created_at, post_created_by, post_image, post_content  FROM blog_post ORDER BY post_created_at DESC');
     $query->execute();
     $blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
     return render('../views/index.php', ['blogPosts'=>$blogPosts]);
 });
+
 $router->get('/admin',function(){
     return render('../views/admin/index.php');
 });
+
 $router->get('/admin/posts/',function() use($pdo){
     $query = $pdo->prepare('SELECT Date_format(post_created_at,"%d-%M-%Y") as post_created_at, post_created_by, post_title FROM blog_post ORDER BY post_created_at,post_created_by ASC');
     $query->execute();
@@ -38,7 +41,21 @@ $router->get('/admin/posts/',function() use($pdo){
     return render('../views/admin/posts.php', ['blogPosts'=>$blogPosts]);
 });
 
+$router->get('/admin/posts/create',function(){
+    return render('../views/admin/insert-post.php');
+});
 
+$router->post('/admin/posts/create',function() use($pdo){
+    $result = false;
+    $sql = "INSERT INTO blog_post (post_title, post_content, post_created_by) VALUES (:title, :content, :author);";
+    $query = $pdo->prepare($sql);
+    $result = $query->execute([
+        'title' => $_POST['inputTitle'],
+        'content' => $_POST['inputContent'],
+        'author' => $_POST['inputAuthor']
+    ]);
+    return render('../views/admin/insert-post.php', ['result'=>$result]);
+});
 
 $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
 
