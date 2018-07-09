724c576e9b077b2acc4ff1d24faf8fa73a906293
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-09 00:39:28 -0400

    psr4 appcontroller

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -11,12 +11,20 @@ class ComposerStaticInit59feb54a1fd2036e9e66eaa8912e6bbb
         array (
             'Phroute\\Phroute\\' => 16,
         ),
+        'A' => 
+        array (
+            'App\\' => 4,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
         'Phroute\\Phroute\\' => 
         array (
             0 => __DIR__ . '/..' . '/phroute/phroute/src/Phroute',
+        ),
+        'App\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/app',
         ),
     );
 
