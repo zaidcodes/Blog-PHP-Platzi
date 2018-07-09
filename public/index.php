68ca09f2a2e49005aae9a0404b6789001afe01d3
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-08 23:36:14 -0400

    Render & baseUrl

--- public/index.php
+++ public/index.php
@@ -4,20 +4,29 @@ ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
 require_once '../vendor/autoload.php';
-
 include_once('../config.php');
 
+$baseUrl = '';
+$baseDir = str_replace(basename($_SERVER['SCRIPT_NAME']),'',$_SERVER['SCRIPT_NAME']); 
+$baseUrl = 'http://' . $_SERVER['HTTP_HOST'] . $baseDir;
+define('BASE_URL',$baseUrl);
+
 $route = $_GET['route'] ?? '/';
+
+function render($fileName, $params = []){
+    ob_start();
+    extract($params);
+    include $fileName;
+    return ob_get_clean();
+}
 
 use Phroute\Phroute\RouteCollector;
 $router = new RouteCollector();
 $router->get('/',function() use($pdo){
     $query = $pdo->prepare('SELECT post_title, Date_format(post_created_at,"%d-%M-%Y") as post_created_at, post_created_by, post_image, post_content  FROM blog_post ORDER BY post_created_at DESC');
     $query->execute();
-
     $blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
-
-    include '../views/index.php';
+    return render('../views/index.php', ['blogPosts'=>$blogPosts]);
 });
 
 $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
