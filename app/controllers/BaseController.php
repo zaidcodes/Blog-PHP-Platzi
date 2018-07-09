8e56da04312cd9caaa6be07ff7d094ffe97ac8f9
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-09 03:02:13 -0400

    Adding a filter

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -10,13 +10,18 @@ class BaseController{
 
     public function __construct(){
         $loader = new Twig_Loader_Filesystem('../views');
-        $this->$templateEngine = new \Twig_Enviroment($loader, [
+        $this->templateEngine = new \Twig_Environment($loader, [
             'debug' => true,
             'cache' => false
         ]);
+
+        $this->templateEngine->addFilter(new \Twig_SimpleFilter('url',function($path){
+            return BASE_URL . $path;
+        }));
     }
 
     public function render($fileName, $data = []){
+        $fileName .= '.twig';
         return $this->templateEngine->render($fileName,$data);
     }
 }
