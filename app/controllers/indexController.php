f247f08192227af4bd04fde2e54f295377492e99
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-09 00:40:44 -0400

    Home controller

---
+++ app/controllers/indexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class IndexController{
+
+    public function getIndex(){
+        global $pdo;
+
+        $query = $pdo->prepare('SELECT post_title, Date_format(post_created_at,"%d-%M-%Y") as post_created_at, post_created_by, post_image, post_content  FROM blog_post ORDER BY post_created_at DESC');
+        $query->execute();
+        $blogPosts = $query->fetchAll(\PDO::FETCH_ASSOC);
+
+        return render('../views/index.php', ['blogPosts'=>$blogPosts]);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -23,12 +23,7 @@ function render($fileName, $params = []){
 use Phroute\Phroute\RouteCollector;
 $router = new RouteCollector();
 
-$router->get('/',function() use($pdo){
-    $query = $pdo->prepare('SELECT post_title, Date_format(post_created_at,"%d-%M-%Y") as post_created_at, post_created_by, post_image, post_content  FROM blog_post ORDER BY post_created_at DESC');
-    $query->execute();
-    $blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
-    return render('../views/index.php', ['blogPosts'=>$blogPosts]);
-});
+$router->controller('/', App\Controllers\IndexController::class);
 
 $router->get('/admin',function(){
     return render('../views/admin/index.php');
