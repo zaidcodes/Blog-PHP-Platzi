765bf543a9786ca2e1f515baff6371567febbb49
Julio Diaz <diaz.cesar.julio@gmail.com>
2018-07-09 23:00:09 -0400

    Users entity and images url default  fixed

--- BBDD/create.sql
+++ BBDD/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE `blog_post`(
     `id`            INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
     `title`         TEXT NOT NULL,
     `content`       LONGTEXT NOT NULL,
-    `image_url`     VARCHAR(300) NOT NULL DEFAULT 'images/unknown.jpg',
+    `image_url`     VARCHAR(300) NOT NULL DEFAULT 'http://localhost/blog/public/images/unknown.jpg',
     `created_by`    VARCHAR(128) NOT NULL DEFAULT 'Anonyous',
     `created_at`    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -15,15 +15,27 @@ CREATE TABLE `user`(
     `name`          VARCHAR(32) NOT NULL,
     `lastname`      VARCHAR(32) NOT NULL,
     `nickname`      VARCHAR(32) NOT NULL,
-    `password`      VARCHAR(128) NOT NULL,   
+    `password`      VARCHAR(128) NOT NULL DEFAULT '$2y$10$7ctSuHFTSv.lZg4t0BZhiO7Qe32Cm/afjePk.blZosPm1HWS42cbG', /*123456*/
     `email`         VARCHAR(128) NOT NULL,
     `created_at`    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `active`        TINYINT(1) NOT NULL DEFAULT '1', 
+    `active`        TINYINT(1) NOT NULL DEFAULT 1, 
     PRIMARY KEY (`id`),
     UNIQUE KEY `user_email` (`email`),
     UNIQUE KEY `user_nickname` (`nickname`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+
+INSERT INTO `user`(
+ `name`,
+ `lastname`,
+ `nickname`,
+ `email`)
+VALUES
+('Name1','Lastname1','Nickname1','email1@email.com'),
+('Name2','Lastname2','Nickname2','email2@email.com'),
+('Name3','Lastname3','Nickname3','email3@email.com'),
+('Name4','Lastname4','Nickname4','email4@email.com'),
+('Name5','Lastname5','Nickname5','email5@email.com');
 
 INSERT INTO `blog_post`(
  `title`,
